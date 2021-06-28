24e7f8b2d9616c79b8d4c341d90b313780e7464a
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-06-28 10:36:13 +0100

    db2

--- Conectar.vb
+++ Conectar.vb
@@ -7,3 +7,15 @@
         "password=123.Abc.@;"
 
 End Class
+
+Public Function BuscarDados() As DataTable
+    Dim conexao As SqlConnection = New SqlConnection(StringConection)
+    conexao.Open()
+    Dim comando As SqlComando = New SqlComando(SSQL, conexao)
+    Dim adpt As New SqlDataAdapter(comando)
+    Dim dt As New DataTable
+    adpt.fill(dt)
+    conexao.Close()
+    Return dt
+
+End Function
