8c214a2835bb7133df42f1da0ab3413bc94d8aa8
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-06-28 10:27:50 +0100

    db

---
+++ Conectar.vb
@@ -0,0 +1,9 @@
+﻿Public Class Conectar
+
+    Private StringConection As String =
+        "Data Source = DOMINGOS\SQLEXPRESS;" +
+        "Initial Catalog = adamastor2;" +
+        "user id= sa;" +
+        "password=123.Abc.@;"
+
+End Class
