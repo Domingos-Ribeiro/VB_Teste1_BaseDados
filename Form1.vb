346c9c623e402468448f679b51ea82c6265a4a2d
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-07-10 23:49:57 +0100

    db1

--- Form1.vb
+++ Form1.vb
@@ -150,4 +150,8 @@
 
 
     End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        'Contar todos os prudutos diferentes da base de dados
+    End Sub
 End Class
